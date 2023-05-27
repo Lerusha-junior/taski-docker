6b23388620843dc08ac1b51121cc5fb1c6efdb42
Lerusha-junior <lerush-jukova@yandex.ru>
2023-05-27 22:32:25 +0400

    Add Actions

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,5 @@
-from rest_framework import status
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.response import Response
-
 
 from .models import Task
 from .serializers import TaskSerializer
